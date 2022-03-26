8533eb6eb3a63db371f71341376968eff0a0d090
Casper24162 <82028471+Casper24162@users.noreply.github.com>
2022-03-26 12:46:05 +0100

    Removed 'Hello World', Added 'Hello World HTTP'

--- main.go
+++ main.go
@@ -2,8 +2,15 @@ package main
 
 import (
 	f "fmt"
+	"log"
+	"net/http"
 )
 
 func main() {
+	http.HandleFunc("/", httpHandler)
+	log.Fatal(http.ListenAndServe(":2424", nil))
+}
+
+func httpHandler(w http.ResponseWriter, r *http.Request) {
 	f.Println("Hello World!")
 }
